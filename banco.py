6bc7e6f2d267cdea75e9bb1f4455e687e4577622
Drew Belloc <bellocdrew@proton.me>
2025-04-13 19:11:53 -0300

    adicionado arquivo para a classe Banco

---
+++ banco.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+class Banco:
+
+    connection = None
+    cursor = None
+
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def close(self):
+        pass
