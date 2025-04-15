82ebf0c3a0e460eb0c1af8fabbb9fcf50c4c9b0e
Drew Belloc <bellocdrew@proton.me>
2025-04-15 19:33:02 -0300

    Adicionei alguns comentários

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return render_template("script.html")
+# Rota para a tela padrão
+@app.route("/") # Define a URL que o cliente irá acessar
+def index(): # Função que será ativada quando o cliente acessar a URL acima
+    return render_template("script.html") # Faz a construção da pagina e retorna para  cliente no navegador
 
-if __name__=="__main__":
-    main()
+if __name__=="__main__": # Faz uma checagem para confirmar que esse é o modulo principal
+    main() # Inicia o servidor
