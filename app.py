b8ec4ea57eeffff55d99740ec25f2586403f49eb
Drew Belloc <bellocdrew@proton.me>
2025-04-21 21:16:40 -0300

    Consertado o app.py e o banco.py

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
+from banco import Banco
 from flask import Flask, render_template
-import sqlite3
 
-app = Flask(__name__)  # TEM QUE vir antes de qualquer @app.route
+app = Flask(__name__)
+db = Banco()
+
+# Rota para a tela padrão
+
+
+@app.route("/")  # Define a URL que o cliente irá acessar
+def index():  # Função que será ativada quando o cliente acessar a URL acima
+    return render_template("script.html")
+
 
 @app.route("/teste")
 def test():
-    connection = sqlite3.connect("banco.db")
-    cursor = connection.cursor()
+    db.connect()
 
     # Busca só os alunos
-    cursor.execute("SELECT nome FROM alunos")
-    alunos = [{"nome": row[0], "tipo": "Aluno"} for row in cursor.fetchall()]
-
-    connection.close()
+    db.cursor.execute("SELECT nome FROM alunos")
+    alunos = [
+        {"nome": row[0], "tipo": "Aluno"}
+        for row in db.cursor.fetchall()
+    ]
+    db.close()
 
     # Ordena os alunos por nome
     alunos.sort(key=lambda x: x["nome"])
 
     return render_template("teste.html", itens=alunos)
+
+
+# checagem confirmando que esse é o modulo principal
+if __name__ == "__main__":
+    app.run()  # Inicia o servidor flask.
